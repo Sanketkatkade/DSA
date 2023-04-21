37110a536da770a7ecbea6768332b8793a94a729
Sanket <sanketkatkade782@gmail.com>
2023-04-21 17:12:39 +0530

    adding new file

---
+++ 03].String/002_string.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // char str[] = "gfg";
+    char str[] = {'g', 'f', 'g', '\0'};
+    cout << str;
+
+    // string str;
+    // cout<<"Enter your name"<<endl;
+    // getline(cin,str);
+    // cout<<"Your name is "<<str<<endl;
+
+    // char s1[] = "abc";
+    // char s2[] = "bcd";
+    // int res = strcmp(s1, s2);
+    // if (res > 0)
+    // {
+    //     cout << "Greater";
+    // }
+    // else if (res == 0)
+    // {
+    //     cout << "Same";
+    // }
+    // else
+    // {
+    //     cout << "Smaller";
+    // }
+
+    // string str = "geeksforgeeks";
+    // cout<<str.length()<<endl;
+    // str = str + "xyz";
+    // cout<<str<<endl;
+    // cout<<str.substr(1,3)<<endl;
+    // cout<<str.find("eek")<<endl;
+
+    // string s1 = "abc";
+    // string s2 = "bcd";
+
+    // if(s1 == s2){
+    //     cout<<"Same"<<endl;
+    // }
+    // else if(s1 < s2){
+    //     cout<<"Greater"<<endl;
+    // }
+    // else{
+    //     cout<<"Smaller"<<endl;
+    // }
+
+    // string str = "geeksforgeeks";
+    // for (int i = 0; i < str.length(); i++)
+    // {
+    //     cout << str[i];
+    // }
+    // cout << endl;
+    // for (char x : str)
+    // {
+    //     cout << x;
+    // }
+    return 0;
+}
